3f5464d2d5a72644edefb6a9b5ec519891517fc2
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-21 15:29:25 -1000

    [ADD] seleccionando variables

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// * VARIABLES
+const inputEmail = document.querySelector('#email');
+const inputAsunto = document.querySelector('#asunto');
+const inputMensaje = document.querySelector('#mensaje');
+
