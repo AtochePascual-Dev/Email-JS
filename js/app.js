b6f9493f0a6f32485246e98bb741edacf799f278
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-21 15:43:14 -1000

    [ADD] asignar eventos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-// * VARIABLES
+//* VARIABLES
 const inputEmail = document.querySelector('#email');
 const inputAsunto = document.querySelector('#asunto');
 const inputMensaje = document.querySelector('#mensaje');
 
+//* EVENTOS
+document.addEventListener('DOMContentLoaded', () => {
+
+  inputEmail.addEventListener('blur', validarInput);
+  inputAsunto.addEventListener('blur', validarInput);
+  inputMensaje.addEventListener('blur', validarInput);
+
+});
+
+//* FUNCIONES
+const validarInput = (event) => {
+
+}
