af169859a9a1b5484f0f09d8685081391d71303d
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-22 17:48:22 -1000

    [UPDATE] refactor

--- js/app.js
+++ js/app.js
@@ -13,21 +13,14 @@ const email = {
 }
 
 
+
 //* EVENTOS
 document.addEventListener('DOMContentLoaded', () => {
   inputEmail.addEventListener('blur', validarInput);
   inputAsunto.addEventListener('blur', validarInput);
   inputMensaje.addEventListener('blur', validarInput);
-
   formulario.addEventListener('submit', enviarEmail);
-
-  btnReset.addEventListener('click', () => {
-    email.email = '';
-    email.asunto = '';
-    email.mensaje = '';
-
-    desactivarBoton();
-  });
+  btnReset.addEventListener('click', formularioReset);
 });
 
 
@@ -35,33 +28,28 @@ document.addEventListener('DOMContentLoaded', () => {
 //* FUNCIONES
 //* Validamos la información obtenida del imput
 const validarInput = (event) => {
-
   const valorInput = event.target.value.trim();
   const tipoInput = event.target.id;
   const referencia = event.target.parentElement;
 
   // validamos si el valor del input es vacio
   if (valorInput === '') {
-    mostrarAlerta(referencia, `El campo ${tipoInput} es obligatorio`, tipoInput);
+    mostrarAlerta(referencia, `El campo ${tipoInput} es obligatorio`);
+    limpiarEmail(tipoInput);
     return;
   }
 
-  // Validamos si el el tipo de input es email
-  if (tipoInput === 'email') {
-    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-
-    // validamos el email
-    if (!regex.test(valorInput)) {
-      mostrarAlerta(referencia, `Email no valido`, tipoInput);
-      return;
-    }
-
+  // Validamos si el el tipo de input es email y el email es incorrecto
+  if (tipoInput === 'email' && !validarEmailInput(valorInput)) {
+    mostrarAlerta(referencia, `Email no valido`);
+    limpiarEmail(tipoInput);
+    return;
   }
 
-  // Si pasa la validacion eliminamos la alerta
+  // Si pasa la validaciones eliminamos la alerta
   eliminarAlerta(referencia, tipoInput);
 
-  // Agregamos la informcion al objeto email
+  // Agregamos la informacion al objeto email
   email[tipoInput] = valorInput;
 
   // Validamos que el email contenga informcion en todas las propiedades
@@ -73,9 +61,9 @@ const validarInput = (event) => {
 
 
 //* Muestra una alerta
-const mostrarAlerta = (referencia, mensaje, tipoInput) => {
-
-  eliminarAlerta(referencia, tipoInput);
+const mostrarAlerta = (referencia, mensaje) => {
+  // Eliminamos la alert previa en caso de existir
+  eliminarAlerta(referencia);
 
   // Creamos la alerta
   const alerta = document.createElement('P');
@@ -89,17 +77,13 @@ const mostrarAlerta = (referencia, mensaje, tipoInput) => {
 
 
 //* Eliminar alerta
-const eliminarAlerta = (referencia, tipoInput) => {
-
+const eliminarAlerta = (referencia) => {
   // Buscamos la alerta en la referencia y la eliminamos
   const existeAlert = referencia.querySelector('.error');
 
   if (existeAlert) {
     existeAlert.remove();
   }
-
-  // Limpiamos la propiedad
-  email[tipoInput] = "";
 
   // desactivamos el boton
   desactivarBoton()
@@ -138,30 +122,50 @@ const enviarEmail = (event) => {
   spinner.classList.remove('hidden');
   spinner.classList.add('flex');
 
-  // Eliminamos las classes que muestran el spiner
+  // Eliminamos las classes que muestran el spinner
   setTimeout(() => {
     spinner.classList.add('hidden');
     spinner.classList.remove('flex');
 
-    email.email = '';
-    email.asunto = '';
-    email.mensaje = '';
+    formularioReset();
 
-    desactivarBoton();
-    formulario.reset();
+    mostrarMensajeExito();
+  }, 2000);
+};
 
-    // Creamos un mesaje de exito
-    const alerta = document.createElement('P');
-    alerta.textContent = "Mensaje enviado correctamente";
-    alerta.className = "p-2 bg-green-500 text-white text-center uppercase text-sm font-bold alerta"
+// * Resetea el formulario
+const formularioReset = () => {
+  email.email = '';
+  email.asunto = '';
+  email.mensaje = '';
 
-    // Mostramos la alerta
-    formulario.appendChild(alerta);
+  desactivarBoton();
+  formulario.reset();
+};
 
-    // Eliminamos la alerta
-    setTimeout(() => {
-      formulario.querySelector('.alerta').remove();
-    }, 2000);
+// * Limpia la propiedad del emai
+const limpiarEmail = (tipoInput) => {
+  email[tipoInput] = '';
+};
 
+// *Validamos si el email del imput es correcto
+const validarEmailInput = (valorInput) => {
+  const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  return regex.test(valorInput);
+}
+
+// * Muestra un mensaje de Exito
+const mostrarMensajeExito = () => {
+  // Creamos un mesaje de exito
+  const alerta = document.createElement('P');
+  alerta.textContent = "Mensaje enviado correctamente";
+  alerta.className = "p-2 bg-green-500 text-white text-center uppercase text-sm font-bold alerta"
+
+  // Mostramos el mensaje
+  formulario.appendChild(alerta);
+
+  // Eliminamos el mensaje
+  setTimeout(() => {
+    formulario.querySelector('.alerta').remove();
   }, 2000);
 };
